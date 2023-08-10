21bd4a9c5bea08b8deaecc582bfbef7708355016
braynguyen <btn16@pitt.edu>
2023-08-10 15:11:23 -0400

    minor changes

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./header.css"
+import img from './responsive-logo.png'
 
 export default function Header() {
   return (
     <div className="group-2215">
       <img
-        src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/0b33xqzbsbuu-502%3A1182?alt=media&token=deeee525-998e-49fd-83a0-c94a258a79df"
+        src={img}
         alt="logo"
         id="logo"
       />
